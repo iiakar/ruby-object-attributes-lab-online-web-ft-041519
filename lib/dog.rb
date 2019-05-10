8a8b4a888762bcef7deb3678d00f7534010b01b4
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-10 14:33:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,5 @@
 class Dog 
 
-
 def name=(name)
     @name = name
   end
@@ -9,4 +8,11 @@ def name=(name)
     @name
   end
   
+  def breed=(breed)
+    @breed = breed 
+  end 
+  
+  def breed
+    @breed
+  end 
 end 
